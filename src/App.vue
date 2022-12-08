8e2ab9e1a430992a9089a4eaf8f5a8554c319f5c
ptsinh <cccfmcf@gmail.com>
2022-12-08 17:05:51 +0700

    x

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@ export default {
     },
     async update() {
       try {
-        await fetch(url, {method: 'POST', body: '{"bearer": "'+this.items.accessToken+'"}'})
+        await fetch(url, {method: 'PUT', body: '{"bearer": "bearer '+this.items.accessToken+'"}'})
       } catch (e) {
         console.error(e.message);
       }
